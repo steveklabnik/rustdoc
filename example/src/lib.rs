798da62693f7029792d4b70d954612f4657a1355
steveklabnik <steve@steveklabnik.com>
2017-05-18 22:26:21 -0400

    Initial commit
    This doesn't work, based @nrc plz halp

---
+++ example/src/lib.rs
@@ -0,0 +1,13 @@
+/// An example struct.
+///
+/// This is mostly so that we can check the docs.
+pub struct ExampleStruct;
+
+impl ExampleStruct{
+    /// An example method.
+    ///
+    /// Sup.
+    pub fn example_method(&self) {
+        println!("Hello world");
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,39 @@
+extern crate rls_analysis as analysis;
+
+use std::env;
+use std::process::Command;
+
+fn main() {
+    let mut command = Command::new("cargo");
+
+    command.arg("build");
+
+    command.env("RUSTFLAGS", "-Z save-analysis");
+    command.env("CARGO_TARGET_DIR", "target/rls");
+
+    println!("running {:?}", command);
+    command.spawn().unwrap().wait().unwrap();
+    println!("done");
+
+    let host = analysis::AnalysisHost::new(analysis::Target::Debug);
+    let dir = &env::current_dir().unwrap();
+    host.reload(dir, dir, true).unwrap();
+
+    let roots = host.def_roots().unwrap();
+
+    //println!("Found these roots: {:?}", roots);
+
+    let &(id, _) = roots.iter().find(|&&(id, ref name)| {
+        println!("{}, {}", id, name);
+        name == "example"
+    }).unwrap();
+
+    println!("id {}", id);
+
+    println!("has def {}", host.has_def(id));
+
+    host.for_each_child_def(id, |id, def| {
+//        println!("id {}, def {:?}", id, def);
+    });
+    
+}
