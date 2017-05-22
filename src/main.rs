796d0a191131cfd3744ef86b2076772a332837d2
steveklabnik <steve@steveklabnik.com>
2017-05-22 18:00:19 -0400

    more stuff

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 extern crate rls_analysis as analysis;
 
 use std::env;
-use std::process::Command;
+use std::io;
+use std::io::prelude::*;
+use std::process::{Command, Stdio};
 
 fn main() {
     let mut command = Command::new("cargo");
@@ -11,29 +13,32 @@ fn main() {
     command.env("RUSTFLAGS", "-Z save-analysis");
     command.env("CARGO_TARGET_DIR", "target/rls");
 
-    println!("running {:?}", command);
-    command.spawn().unwrap().wait().unwrap();
-    println!("done");
+    // for now, just eat the output
+    command.stdout(Stdio::null());
+    command.stderr(Stdio::null());
 
+    print!("generating save analysis data...");
+    io::stdout().flush().unwrap();
+
+    command.spawn().unwrap().wait().unwrap();
+    println!("done.");
+
+    print!("loading save analysis data...");
+    io::stdout().flush().unwrap();
     let host = analysis::AnalysisHost::new(analysis::Target::Debug);
     let dir = &env::current_dir().unwrap();
     host.reload(dir, dir, true).unwrap();
+    println!("done.");
 
     let roots = host.def_roots().unwrap();
 
-    //println!("Found these roots: {:?}", roots);
-
-    let &(id, _) = roots.iter().find(|&&(id, ref name)| {
-        println!("{}, {}", id, name);
+    let &(id, _) = roots.iter().find(|&&(_, ref name)| {
         name == "example"
     }).unwrap();
 
-    println!("id {}", id);
-
-    println!("has def {}", host.has_def(id));
-
-    host.for_each_child_def(id, |id, def| {
-        println!("id {}, def {:?}", id, def);
+    host.for_each_child_def(id, |_, def| {
+        println!("{}", def.name);
+        println!("{}", def.docs);
     }).unwrap();
     
 }
