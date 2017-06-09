e1a0439a0d07f977256c20eb90ad387b8b037a06
steveklabnik <steve@steveklabnik.com>
2017-06-09 14:35:20 -0400

    hi glimmer

---
+++ frontend/config/environment.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function(environment) {
+  let ENV = {
+    modulePrefix: 'frontend-app',
+    environment: environment
+  };
+
+  return ENV;
+};
---
+++ frontend/src/ui/components/frontend-app/component.ts
@@ -0,0 +1,5 @@
+import Component from "@glimmer/component";
+
+export default class Frontend extends Component {
+
+}
