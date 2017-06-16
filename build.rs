d61967cd7f0307f1c1ab5639134754118d41a9a5
steveklabnik <steve@steveklabnik.com>
2017-06-16 18:07:38 -0400

    oh god this build.rs is specific to me

---
+++ build.rs
@@ -0,0 +1,21 @@
+use std::process::Command;
+
+fn main() {
+    let mut output = Command::new(r"C:\Program Files (x86)\Nodist\bin\ember.cmd");
+
+    output.current_dir("frontend");
+
+    output.arg("build");
+    output.arg("--prod");
+
+    output.arg("-o");
+    output.arg("../target/frontend");
+
+    let output = output.output().unwrap();
+
+    if !output.status.success() {
+        panic!("build script failed with status {}. stderr:\n{}", output.status, String::from_utf8_lossy(&output.stderr));
+    }
+    
+    println!("cargo:rerun-if-changed=frontend")
+}
