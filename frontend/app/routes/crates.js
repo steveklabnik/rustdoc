2d9e28cafaf6572c5a72ee3273f77c6c7f23bbed
steveklabnik <steve@steveklabnik.com>
2017-06-19 20:05:33 -0400

    remove superflous alert
    this was for debugging purposes

--- frontend/app/routes/crates.js
+++ frontend/app/routes/crates.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model(params) {
-        alert(this.get('store').peekRecord('crate', params.crate_name));
         return this.get('store').peekRecord('crate', params.crate_name);
     }
 });
