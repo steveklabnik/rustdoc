c62c77ffbf8ed52b9968950f99f5a4d8bb35d616
steveklabnik <steve@steveklabnik.com>
2017-07-26 11:31:21 -0400

    Start of frontend work for nested things
    This commit does three things:
    
    1. modifies data.json to include a nested child module
    2. modifies app/models/module.js to include parent/child module
       relationships
    3. modifies app/templates/modules.hbs to display links to child modules

--- frontend/app/routes/modules.js
+++ frontend/app/routes/modules.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model({ crate_name, path }) {
-        return this.get('store').peekRecord('module', `${crate_name}::${path}`);
-    }
+  // serialize is called whenever you want to turn this route's model into a
+  // URL. For this reason, we need to turn `::`s into `/`s when producing URLs.
+  serialize(mod) {
+    return { path: mod.get('id').replace(/::/g, '/') };
+  },
+
+  // To find the model from the URL, we need to convert /s to ::s, since the
+  // URLs have /s but the IDs have ::s.
+  model(params) {
+    return this.store.peekRecord('module', params.path.replace(/\//g, '::'));
+  }
 });
