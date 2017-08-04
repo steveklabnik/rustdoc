3205821b4c2521288f77f93ef0b7770be184ba44
steveklabnik <steve@steveklabnik.com>
2017-08-04 18:30:13 -0400

    fix tidy

--- tests/source/nested_modules.rs
+++ tests/source/nested_modules.rs
@@ -4,8 +4,8 @@
 // @has /included/0/relationships/child_modules/data/0/id 'nested_modules::example_module::nested'
 mod example_module {
 
-    // @has /included/1/relationships/child_modules/data/0/type 'module'
-    // @has /included/1/relationships/child_modules/data/0/id 'nested_modules::example_module::nested::nested2'
+// @has /included/1/relationships/child_modules/data/0/type 'module'
+// @has /included/1/relationships/child_modules/data/0/id 'nested_modules::example_module::nested::nested2'
     mod nested {
         mod nested2 {}
     }
